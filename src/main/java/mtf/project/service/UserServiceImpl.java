0187165af2bd817f8aedd7ac166fdf52abfe7f96
adisryn10 <imadeadisuryanugraha@gmail.com>
2020-01-05 01:55:42 +0700

    add:controller, repository and service

---
+++ src/main/java/mtf/project/service/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package mtf.project.service;
+
+import mtf.project.model.*;
+import mtf.project.repository.*;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDb userDb;
+
+    @Override
+	public List<UserModel> getAllUser() {
+		return userDb.findAll();
+    }
+
+    @Override
+    public UserModel getUserById(String id) {
+        return userDb.findById(id);
+    }
+}
